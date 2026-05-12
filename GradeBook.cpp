1963b2da043aec336a3ee8fad549e1cdad513454
Erol1978 <30065105@cc.peralta.edu>
2026-05-11 23:49:50 -0700

    GradeBook.cpp

---
+++ GradeBook.cpp
@@ -0,0 +1,24 @@
+cat > GradeBook.cpp <<'EOF'
+#include "GradeBook.h"
+
+GradeBook::GradeBook() {
+}
+
+void GradeBook::addGrade(const std::string& assignment, double score) {
+}
+
+double GradeBook::getAverage() const {
+    return 0.0;
+}
+
+double GradeBook::getHighest() const {
+    return 0.0;
+}
+
+int GradeBook::numGrades() const {
+    return 0;
+}
+EOF
+
+git add GradeBook.cpp
+git commit -m "Add stub implementations"
